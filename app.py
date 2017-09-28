98f820ac6046483418ecc990b9761d18eac1c620
Tanuj Srivastava <srim0987@gmail.com>
2017-09-28 14:31:51 +0530

    adding font list

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from PIL import Image, ImageDraw, ImageFont
 import requests, textwrap, csv, random, StringIO, base64, io
 from flask import send_file, render_template
 
-_quote_font = ImageFont.truetype('Lato-Regular.ttf', 24)
-_author_font = ImageFont.truetype('Lato-Regular.ttf', 14)
+def getFont():
+	fontl = ['Lato-Regular.ttf', 'Pacifico-Regular.ttf', 'Lobster-Regular.ttf', 'GermaniaOne-Regular.ttf']
+	num = random.randint(0,3)
+	fnt = fontl[num]
+	response = {}
+	response['quote_font'] = ImageFont.truetype(fnt, 24)
+	response['author_font'] = ImageFont.truetype(fnt, 14)
+	return response
 
 #method to fetch quotes
 def getQuote():
@@ -31,18 +37,23 @@ def getImage():
 	pick_url = random.randint(1,10)
 	img_url = data['hits'][pick_url]['webformatURL']
 	#returns image url to processImage method
+	print img_url
 	return img_url
 
 #method to process image
-def prcoessImage(img, quote, qfont=_quote_font, afont=_author_font):
+def prcoessImage(img, quote):
 	#opening image for work.
-	im = Image.open(img)
+	im = Image.open('fff3.png')
 	width, height = im.size
+	print width
 	while width < 640:
+		print 'reach'
 		img_url = getImage()
+		print img_url
 		img = requests.get(img_url, stream=True).raw
 		im = Image.open(img)
 		width, height = im.size
+	print width, height
 	cropDim = im.size[1]
 	left = (im.size[0]/2) - (cropDim/2)
 	right = (im.size[0]/2) + (cropDim/2)
@@ -54,6 +65,9 @@ def prcoessImage(img, quote, qfont=_quote_font, afont=_author_font):
 	quoteText = textwrap.wrap(quote['quote'], width = 25)
 	quoteAuthor = quote['author']
 	#writing text
+	fnt = getFont()
+	qfont = fnt['quote_font']
+	afont = fnt['author_font']
 	draw = ImageDraw.Draw(im)
 	x, y = 20, cropDim/8
 	for text in quoteText:
